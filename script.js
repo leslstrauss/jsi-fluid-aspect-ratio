a59eaac567cad05daad9516892687f9fd0fa282f
nonogon <leslie.strauss@gmail.com>
2014-10-01 10:51:42 -0700

    ajax request added

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-$.ajaxSetup ({
-    cache: false
-  });
-  var ajax_load = "<img src='img/load.gif' alt='loading...' />";
+// $.ajaxSetup ({
+//     cache: false
+//   });
+//   var ajax_load = "<img src='img/load.gif' alt='loading...' />";
 
-//  load() functions
-  var loadUrl = "ajax/load.php";
-  $("#load_basic").click(function(){
-    $("#result").html(ajax_load).load(loadUrl);
-  });
+// //  load() functions
+//   var loadUrl = "ajax/load.php";
+//   $("#load_basic").click(function(){
+//     $("#result").html(ajax_load).load(loadUrl);
+//   });
+
+$.ajax({
+   url: "package.json",
+   dataType : "json",
+   success: function( json ) {
+       console.log(json);
+     $( "<h1/>" ).text( json.name ).appendTo( "body" );
+   }
+});
